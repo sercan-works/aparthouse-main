517c51a386a10967773af97861f7d944f25e4d3c
Sercan <sercantas1026@gmail.com>
2025-08-06 21:02:42 +0300

    tag manager kaldırıldı

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,18 +48,7 @@ export default function RootLayout({
             gtag('config', 'G-N8XHCSGGSM');
           `}
         </Script>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=GTM-KSLXLKB9"
-          strategy="afterInteractive"
-        />
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'GTM-KSLXLKB9');
-          `}
-        </Script>
+       
         
         <ReduxProvider>
           <AuthProvider>
