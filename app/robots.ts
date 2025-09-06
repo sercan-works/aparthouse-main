499b6a2f26fac9158cd1dfaac579a763e255f9a1
Sercan <sercantas1026@gmail.com>
2025-09-06 23:25:54 +0300

    ff

--- app/robots.ts
+++ app/robots.ts
@@ -7,13 +7,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/api/', '/admin/'],
       },
-      {
-        userAgent: 'Googlebot',
-        allow: '/',
-        disallow: ['/api/', '/admin/'],
-      }
     ],
     sitemap: 'https://aparthouse.com.tr/sitemap.xml',
   };
